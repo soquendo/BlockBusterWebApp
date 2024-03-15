eef680a9fffab0efca9c1e6803aaff92a63b71ef
Sebastian Oquendo <soquendo@email.neit.edu>
2024-03-15 02:51:33 -0400

    Add project files.

---
+++ BlockBusterWebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using BlockBusterWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlockBusterWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Colors()
+        {
+            string[] colors = { "red", "blue", "yellow" };
+            ViewBag.Colors = colors;
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            string[] cities = { "Berlin", "Palermo", "Nairobi", "Helsinki", "Bogota" };
+            ViewBag.Cities = cities;
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            string[] hobbies = { "Photography", "Graphic Design", "Cooking", "Video Games", "Sports" };
+            ViewBag.Hobbies = hobbies;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BlockBusterWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlockBusterWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
